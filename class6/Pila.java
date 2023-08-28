84175100532138cceeac204624e815e381bc0d07
yerkortiz <yerko.ortiz@sumup.com>
2023-08-28 09:49:18 -0400

    class6

---
+++ class6/Pila.java
@@ -0,0 +1,55 @@
+package class6;
+
+public class Pila {
+    LinkedListNode top;
+
+    static class LinkedListNode {
+        int value;
+        LinkedListNode next;
+
+        LinkedListNode(int x) {
+            this.value = x;
+        }
+
+        LinkedListNode(int x, LinkedListNode n) {
+            this.value = x;
+            this.next = n;
+        }
+    }
+
+    void push(int x){
+        if(this.top == null){
+            this.top = new LinkedListNode(x);
+            return;
+        }
+
+        LinkedListNode aux = new LinkedListNode(x);
+        aux.next = this.top;
+        this.top = aux;
+    }
+
+    int pop(){
+        if(this.top == null){
+            return -1;
+        }
+
+        int x;
+        x = this.top.value;
+        if(this.top.next == null){
+            this.top = null;
+            return x;
+        }
+        this.top = this.top.next;
+        return x;
+    }
+    public static void main(String[] args) {
+        Pila p = new Pila();
+        p.push(1);
+        p.push(2);
+        p.push(3);
+        System.out.println(p.pop());
+        System.out.println(p.pop());
+        System.out.println(p.pop());
+        System.out.println(p.pop());
+    }
+}
